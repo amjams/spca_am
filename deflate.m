1f9650385eefb286defa1b00608ea26595515fbb
Ahmad Alsahaf <ahmadalsahaf@gmail.com>
2014-07-11 12:53:32 +0200

    Update deflate.m

--- deflate.m
+++ deflate.m
@@ -4,12 +4,14 @@ function [ At ] = deflate(A,x)
 %  x is the leading eigen vector of the matrix
 %  At is thr resulting covariance matrix after deflation with x
 %
-% Refer to spca_am and [1] for details
-% [1] Peter Richtárik,  Martin Taká?, S. Damla Ahipasaoglu. Alternating
+% Refer to 'spca_am', [1] and [2] for details
+% [1] Peter Richtï¿½rik,  Martin Takï¿½?, S. Damla Ahipasaoglu. Alternating
 % Maximization: Unifying Framework for 8 Sparse PCA Formulations and
 % Efficient Parallel Codes
 % https://code.google.com/p/24am/
 %
+% [2] http://wikicoursenote.com/wiki/Deflation_Methods_for_Sparse_PCA
+% 
 %
 % Code developed by: Ahmad Alsahaf (ahmadalsahaf@gmail.com)
 % MSc student at Politecnico di Milano
